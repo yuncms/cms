52d12243d468654e1f8fa0d3f927c28181c76853
xutongle <xutongle@gmail.com>
2018-02-08 18:00:56 +0800

    update

--- bootstrap/bootstrap.php
+++ bootstrap/bootstrap.php
@@ -5,12 +5,15 @@
  * @license http://www.tintsoft.com/license/
  */
 
+// Set the vendor path. By default assume that it's 4 levels up from here
+$vendorPath = dirname(__DIR__, 3);
+
 // Load the files
-$cmsPath = $vendorPath.'/yuncms/cms';
-$libPath = $cmsPath.'/lib';
-$srcPath = $cmsPath.'/src';
-require $vendorPath.'/yiisoft/yii2/Yii.php';
-require $srcPath.'/Yun.php';
+$cmsPath = $vendorPath . '/yuncms/cms';
+$libPath = $cmsPath . '/lib';
+$srcPath = $cmsPath . '/src';
+require $vendorPath . '/yiisoft/yii2/Yii.php';
+require $srcPath . '/Yun.php';
 
 // Set aliases
 Yun::setAlias('@lib', $libPath);
