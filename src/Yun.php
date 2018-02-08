49ca3a3c343463dd08413a28ee9cf5bd967c2327
xutongle <xutongle@gmail.com>
2018-02-08 14:50:27 +0800

    init

--- src/Yun.php
+++ src/Yun.php
@@ -5,3 +5,37 @@
  * @license http://www.tintsoft.com/license/
  */
 
+use Yii;
+use GuzzleHttp\Client;
+
+/**
+ * Class Yun
+ *
+ * @author Tongle Xu <xutongle@gmail.com>
+ * @since 1.0
+ */
+class Yun extends Yii
+{
+    /**
+     * Creates a Guzzle client configured with the given array merged with any default values in config/guzzle.php.
+     *
+     * @param array $config Guzzle client config settings
+     *
+     * @return Client
+     */
+    public static function createGuzzleClient(array $config = []): Client
+    {
+        // Set the Craft header by default.
+        $defaultConfig = [
+            'headers' => [
+                'User-Agent' => 'Yun/' . self::$app->getVersion() . ' ' . \GuzzleHttp\default_user_agent()
+            ],
+        ];
+
+        // Maybe they want to set some config options specifically for this request.
+        $guzzleConfig = array_replace_recursive($defaultConfig, $config);
+
+        return new Client($guzzleConfig);
+    }
+
+}
