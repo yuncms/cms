ca6dc4d28be2b641958829784a48694e2afc737a
xutongle <xutongle@gmail.com>
2018-02-08 16:34:18 +0800

    add Request

---
+++ src/web/Request.php
@@ -0,0 +1,37 @@
+<?php
+/**
+ * @link http://www.tintsoft.com/
+ * @copyright Copyright (c) 2012 TintSoft Technology Co. Ltd.
+ * @license http://www.tintsoft.com/license/
+ */
+
+namespace yuncms\web;
+
+/**
+ * Class Request
+ * @package yuncms\web
+ *
+ * @author Tongle Xu <xutongle@gmail.com>
+ * @since 1.0
+ */
+class Request extends \yii\web\Request
+{
+    public $ipHeaders = [
+        'Client-IP',
+        'X-Forwarded-For',
+        'X-Forwarded',
+        'X-Cluster-Client-IP',
+        'X-REAL-IP',
+        'Forwarded-For',
+        'Forwarded',
+        'RemoteIp'
+    ];
+
+    public $secureProtocolHeaders = [
+        'X-Forwarded-Proto' => ['https'],
+        'Front-End-Https' => ['on'],
+        'X-CLIENT-SCHEME' => ['https'],
+        'X-Client-Proto' => ['https'],
+    ];
+
+}
