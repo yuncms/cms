18606779310a0e3233b60dcdec2453e4f3a9ad31
xutongle <xutongle@gmail.com>
2018-02-23 19:14:43 +0800

    update

--- config/mimeTypes.php
+++ config/mimeTypes.php
@@ -1,5 +1,5 @@
 <?php
-$mimeTypes = require Yun::getAlias('@yii/helpers/mimeTypes.php');
+$mimeTypes = require Yii::getAlias('@yii/helpers/mimeTypes.php');
 $mimeTypes['markdown'] = 'text/markdown';
 $mimeTypes['md'] = 'text/markdown';
 $mimeTypes['woff2'] = 'application/font-woff2';
